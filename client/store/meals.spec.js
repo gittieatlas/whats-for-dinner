62d8ba2034b2f4091a73592db611c3609cb1f3b2
Gittie Atlas <gittieatlas@gmail.com>
2018-09-15 23:49:09 -0400

    tests: meals reducer and MealsList component

--- client/store/meals.spec.js
+++ client/store/meals.spec.js
@@ -1,3 +1,83 @@
-// TODO: write tests for meals reducer
+// assertions
+import chai from 'chai'
+const expect = chai.expect
+import chaiThings from 'chai-things'
+chai.use(chaiThings)
 
-// Why does user.spec.js only test thunk creators
+// redux
+import axios from 'axios'
+import MockAdapter from 'axios-mock-adapter'
+import configureMockStore from 'redux-mock-store'
+import thunkMiddleware from 'redux-thunk'
+
+const middlewares = [thunkMiddleware]
+const mockStore = configureMockStore(middlewares)
+
+// meals reducer
+import {SET_MEALS, setMeals, fetchMeals} from './meals'
+
+describe.only('Meals Reducer', () => {
+  let store
+  let mockAxios
+  let actions
+
+  const initialState = {meals: []}
+
+  const fakeMeals = [
+    {
+      name: 'Mediterranean-Spiced Grilled Chicken',
+      shortDescription: 'with roasted sweet potatoes and fattoush salad',
+      longDescription:
+        'A little marinade goes a long way with our flavorful Mediterranean-Spiced Grilled Chicken that can be prepared on the stove or grill. The crisp, roasted sweet potatoes add a savory complement to the spice and the fattoush salad rounds everything out with a refreshing side of veggies.',
+      imageUrl:
+        'https://cdn.shopify.com/s/files/1/0006/0227/5901/products/2018-06-CoCoBox_JenMayPhoto_20_800x.png?v=1531271400',
+      sizes: [
+        {price: '38', size: '2', mealId: 1},
+        {price: '68', size: '4', mealId: 1},
+        {price: '108', size: '6', mealId: 1}
+      ]
+    },
+    {
+      name: 'Dijon Salmon',
+      shortDescription: 'with roasted mushrooms shallots and arugula salad',
+      longDescription:
+        'Salmon and mushrooms are an unlikely, yet beautiful flavor pairing that really sings when baked. This is a hands-off recipe that’ll leave you wondering why you’ve never tried this delightful combo before. But hey, at least you know now!',
+      imageUrl:
+        'https://cdn.shopify.com/s/files/1/0006/0227/5901/products/2018-06-CoCoBox_JenMayPhoto_62_800x.png?v=1531271488',
+      sizes: [
+        {price: '48', size: '2', mealId: 2},
+        {price: '88', size: '4', mealId: 2},
+        {price: '138', size: '6', mealId: 2}
+      ]
+    }
+  ]
+
+  beforeEach(async () => {
+    mockAxios = new MockAdapter(axios)
+    store = mockStore(initialState)
+
+    mockAxios.onGet('/api/meals').replyOnce(200, fakeMeals)
+    await store.dispatch(fetchMeals(fakeMeals))
+    actions = store.getActions()
+  })
+
+  afterEach(() => {
+    mockAxios.restore()
+    store.clearActions()
+  })
+
+  describe('fetch meals', () => {
+    it('eventually dispatches the SET_MEALS action', () => {
+      expect(actions[0]).to.be.deep.equal(setMeals(fakeMeals))
+      expect(actions[0].type).to.be.equal(SET_MEALS)
+      expect(actions[0].meals).to.be.deep.equal(fakeMeals)
+    })
+
+    xit('store has new state with the fetched meals', () => {
+      // TODO: how to test that the state changed?
+      // state.meals is empty array at this point
+      // expect(store.getState().meals).to.deep.equal(fakeMeals)
+      // expect(store.getState().meals).to.have.length(2)
+    })
+  })
+})
