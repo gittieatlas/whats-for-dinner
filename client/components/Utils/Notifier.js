6b423d3a4d5e1b74a2ffc407a9970f7f9463d0da
Gittie Atlas <gittieatlas@gmail.com>
2018-09-16 01:50:24 -0400

    frontend: remove.js from Notifier.css

--- client/components/Utils/Notifier.js
+++ client/components/Utils/Notifier.js
@@ -6,7 +6,7 @@ import IconButton from '@material-ui/core/IconButton'
 import CloseIcon from '@material-ui/icons/Close'
 
 import {withStyles} from '@material-ui/core/styles'
-import styles from './Notifier.css.js'
+import styles from './Notifier.css'
 
 let openSnackbarFn
 
