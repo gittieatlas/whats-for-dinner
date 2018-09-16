d207a38802f7758446e92407098e466811d42907
Gittie Atlas <gittieatlas@gmail.com>
2018-09-16 14:26:15 -0400

    chore: remove references to UserHome component

--- client/routes.js
+++ client/routes.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {withRouter, Route, Switch} from 'react-router-dom'
 import PropTypes from 'prop-types'
-import {Login, Signup, UserHome, MealList} from './components'
+import {Login, Signup, MealList} from './components'
 import {me} from './store'
 
 class Routes extends Component {
