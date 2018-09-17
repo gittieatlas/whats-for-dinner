34243cee6cf9a6e8d3fda8b9e0698142e57db6a8
Gittie Atlas <gittieatlas@gmail.com>
2018-09-17 18:22:33 -0400

    feat(checkout): orders route

---
+++ server/api/orders.js
@@ -0,0 +1,19 @@
+const router = require('express').Router()
+const {Order} = require('../db/models')
+module.exports = router
+
+router.post('/', async (req, res, next) => {
+  try {
+    const {address, phoneNumber, cart, userId} = req.body
+    const orderData = {
+      shippingAddress: address,
+      phoneNumber,
+      items: cart,
+      userId
+    }
+    const order = await Order.create(orderData, {returning: true})
+    res.json(order.id)
+  } catch (err) {
+    next(err)
+  }
+})
