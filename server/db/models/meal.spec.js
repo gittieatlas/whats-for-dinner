003dadbfe5b2592d6ca4429c15d825ccb64bef41
Gittie Atlas <gittieatlas@gmail.com>
2018-09-18 14:30:13 -0400

    chore(meal): add tests for meal model

--- server/db/models/meal.spec.js
+++ server/db/models/meal.spec.js
@@ -4,12 +4,104 @@ const {expect} = require('chai')
 const db = require('../index')
 const Meal = db.model('meal')
 
-describe('Meal model', () => {
-  beforeEach(() => {
-    return db.sync({force: true})
-  })
+describe.only('Meal model', () => {
+  describe('Validations', () => {
+    it('requires name', async () => {
+      const meal = Meal.build({
+        shortDescription: 'with roasted sweet potatoes and fattoush salad',
+        longDescription:
+          'A little marinade goes a long way with our flavorful Mediterranean-Spiced Grilled Chicken that can be prepared on the stove or grill. The crisp, roasted sweet potatoes add a savory complement to the spice and the fattoush salad rounds everything out with a refreshing side of veggies.',
+        imageUrl:
+          'https://cdn.shopify.com/s/files/1/0006/0227/5901/products/2018-06-CoCoBox_JenMayPhoto_20_800x.png?v=1531271400',
+        servings: '2',
+        price: 3800
+      })
 
-  describe('what to test?', () => {
-    // TODO: how do we test models
-  }) // end describe('what to test?')
-}) // end describe('Meal model')
+      try {
+        await meal.validate()
+        throw Error(
+          'validation was successful but should have failed without `name`'
+        )
+      } catch (err) {
+        expect(err.message).to.contain('name cannot be null')
+      }
+    })
+
+    it('requires name to not be an empty string', async () => {
+      const meal = Meal.build({
+        name: '',
+        shortDescription: 'with roasted sweet potatoes and fattoush salad',
+        longDescription:
+          'A little marinade goes a long way with our flavorful Mediterranean-Spiced Grilled Chicken that can be prepared on the stove or grill. The crisp, roasted sweet potatoes add a savory complement to the spice and the fattoush salad rounds everything out with a refreshing side of veggies.',
+        imageUrl:
+          'https://cdn.shopify.com/s/files/1/0006/0227/5901/products/2018-06-CoCoBox_JenMayPhoto_20_800x.png?v=1531271400',
+        servings: '2',
+        price: 3800
+      })
+
+      try {
+        await meal.validate()
+        throw Error(
+          'validation was successful but should have failed if name is an empty string'
+        )
+      } catch (err) {
+        expect(err.message).to.contain('Validation error')
+      }
+    })
+
+    it('requires price', async () => {
+      const meal = Meal.build({
+        name: 'Mediterranean-Spiced Grilled Chicken',
+        shortDescription: 'with roasted sweet potatoes and fattoush salad',
+        longDescription:
+          'A little marinade goes a long way with our flavorful Mediterranean-Spiced Grilled Chicken that can be prepared on the stove or grill. The crisp, roasted sweet potatoes add a savory complement to the spice and the fattoush salad rounds everything out with a refreshing side of veggies.',
+        imageUrl:
+          'https://cdn.shopify.com/s/files/1/0006/0227/5901/products/2018-06-CoCoBox_JenMayPhoto_20_800x.png?v=1531271400',
+        servings: '2'
+      })
+
+      try {
+        await meal.validate()
+        throw Error(
+          'validation was successful but should have failed without `price`'
+        )
+      } catch (err) {
+        expect(err.message).to.contain('price cannot be null')
+      }
+    })
+
+    it('should have a servings property of either "2", "4", or "6" (nothing else)', async () => {
+      const meal = Meal.build({
+        name: 'Mediterranean-Spiced Grilled Chicken',
+        shortDescription: 'with roasted sweet potatoes and fattoush salad',
+        longDescription:
+          'A little marinade goes a long way with our flavorful Mediterranean-Spiced Grilled Chicken that can be prepared on the stove or grill. The crisp, roasted sweet potatoes add a savory complement to the spice and the fattoush salad rounds everything out with a refreshing side of veggies.',
+        imageUrl:
+          'https://cdn.shopify.com/s/files/1/0006/0227/5901/products/2018-06-CoCoBox_JenMayPhoto_20_800x.png?v=1531271400',
+        servings: '2',
+        price: 3800
+      })
+
+      // confirming these work fine
+      await meal.save()
+      meal.servings = '4'
+      await meal.save()
+      meal.servings = '6'
+      await meal.save()
+
+      // confirming this doesn't work at all
+      try {
+        meal.servings = '8'
+        await meal.save()
+      } catch (err) {
+        expect(err.message).to.contain('servings')
+        return // everything is fine, so stop this spec.
+      }
+
+      // if we got here, that means we DIDN'T fail above, which is wrong.
+      throw Error(
+        'Trying to `save` a meal with invalid `servings` should have failed.'
+      )
+    })
+  })
+})
